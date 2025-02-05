23d4acc82d001b4acd35af8363ea8a026f328138
Onuoha Collins <onuohacollinschidubem@gmail.com>
2025-02-05 09:32:50 +0100

    new changes

--- app/data.js
+++ app/data.js
@@ -4,6 +4,4 @@ export const products = [
   { id: 3, name: "Coffee Maker", category: "Appliances", price: "$99" },
   { id: 4, name: "Headphones", category: "Accessories", price: "$199" },
   { id: 4, name: "Phone", category: "Accessories", price: "$199" },
-  { id: 5, name: "Desk Chair", category: "Furniture", price: "$149" },
-  { id: 6, name: "Desk", category: "Furniture", price: "$159" },
 ];
